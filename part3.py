ce684b661779216696cb136e7550618271f7dfab
Doreen <doreen.nps@gmail.com>
2020-12-02 18:26:49 +0800

    Part 3 kinda works now, kill me pls

--- part3.py
+++ part3.py
@@ -24,12 +24,12 @@ class Set:
         df = pd.DataFrame({'words': words, 'tags': tags}, columns=['words', 'tags'])
         self.training = df
 
+        f.close()
+
 
 class HMM:
     def __init__(self, training_dataset=None):
         self.training_set = training_dataset.training
-        # tg = self.training_set.tags.unique()
-        # self.tags = np.append(tg, 'S')
         self.tags = self.training_set.tags.unique()
         self.words = self.training_set.words.unique()
 
@@ -60,76 +60,51 @@ class HMM:
         return q
 
     def train_trans_params(self):
-        print(self.tags)
+
         yparams = []
         y = []
         print('training...')
         for tag in self.tags:
-            print('each tag prob')
+
             prob = []
-            given_prev_y = []
             for next_tag in self.tags:
-
                 q = self.trans_params(tag, next_tag)
-                print('next tag prob: ' + str(q))
                 prob.append(q)
-                print('appending probability')
-                # given_prev_y.append([tag, next_tag])
-            yparams.append(prob)
-            print('appending list of probabilities for each tag: ' + str(prob))
-            y.append(str(tag))
-            print('appending tag(y): ' + tag)
 
-        # x = []
-        # for i in range(len(self.tags)-1):
-        #     pair = [self.words[i], self.words[i+1]]
-        #     x.append(pair)
-        # y = []
-        # for i in self.tags:
-        #     y.append(i)
-        dicti = {'tags:': y, 'y_params': yparams}
-        print(dicti)
-        df = pd.DataFrame(dicti, columns={'tags', 'y_params'})
-        print('stored in dataframe\n')
-        print(df.values[0])
-        print(df)
-        # df = pd.DataFrame({'words': x, 'tags:': y, 'y_params': yparams}, columns={'words', 'tags', 'y_params'})
+            yparams.append(prob)
+            y.append(str(tag))
+
+        df = pd.DataFrame({'tags': y, 'y_params': yparams}, columns={'tags', 'y_params'})
         self.transi_params = df
-        print(df['tags'])
 
     def set_params(self, dfx, dfy):
         self.emission_params = dfx
         self.transistion_params = dfy
-        print(dfy['tags'])
 
     def max_b(self, word):
         x = word
-        if x == '':
-            max_b_val = 1
+
+        if x in self.words:
+            x1 = x
         else:
-            if x in self.words:
-                x1 = x
-            else:
-                x1 = '#UNK#'
-            row = self.emission_params.loc[self.emission_params['words'] == x1]
-            probs = row['params'].values[0]
-            max_b_val = max(probs)
-            # pos = np.argmax(probs)
-            # y = self.tags[pos]
+            x1 = '#UNK#'
+
+        row = self.emission_params.loc[self.emission_params['words'] == x1]
+        probs = row['params'].values[0]
+
         return probs
 
     def max_a(self, prev_y):
-        # if x in self.transistion_params['words']:
         row = self.transistion_params.loc[self.transistion_params['tags'] == prev_y]
         probs = row['y_params'].values[0]
-        # pos = np.argmax(probs)
-        # y = self.tags[pos]
+
         return probs
 
     def viterbi(self, in_file, out_file):
         f_in = open(in_file, 'r')
         f_out = open(out_file, 'w')
-        # words = [x.strip() for x in f_in.readlines()]
+        print('Viterbi running...')
+
         words = ['\n']
         lines = f_in.readlines()
         for i in lines:
@@ -138,46 +113,56 @@ class HMM:
             else:
                 words.append(i.strip())
 
-        gen_tag = list(words)
         newtags_list = []
 
         for i in range(len(words)):
             if words[i] == '\n':
                 scores = []
                 pi_j = 1
+                some_tag = 'S'
                 for j in range(i, len(words) - 1):
-                    a_probs = self.max_a(words[j])
+                    a_probs = self.max_a(some_tag)
                     b_probs = self.max_b(words[j+1])
                     ab = []
                     for k, l in zip(a_probs, b_probs):
                         ab.append(k*l)
                     pi_j = pi_j * max(ab)
                     pos = np.argmax(ab)
-                    new_tags = self.tags[pos]
-                    newtags_list.append(new_tags)
+                    new_tag = self.tags[pos]
+                    newtags_list.append(new_tag)
 
-                    # b_score = self.max_b(words[j + 1])
-                    # a_score = self.max_a(words[j])
-                    # pi_j = pi_j * b_score * a_score
+                    some_tag = new_tag
                     scores.append(pi_j)
-                    # gen_tag[j] = new_tag
 
-                    if words[j+1] == '\n':
-                        last_score = max(self.max_a(words[j + 1]))
+                    if words[j+2] == '\n':
+                        last_score = max(self.max_a(some_tag))
                         pi_n1 = pi_j * last_score
                         scores.append(pi_n1)
-                        pos = np.argmax(self.max_a(words[j+1]))
+                        pos = np.argmax(self.max_a(some_tag))
                         newtags_list.append(self.tags[pos])
                         break
 
-            # scores_list.append(scores)
-
-        # for i in range(len(scores_list)-1, 0, -1):
-        #     for j in range(len(scores_list[i])-1, 0, -1):
-
         generated_tags = ['' if k == 'S' else k for k in newtags_list]
-        for i, j in zip(f_in.readlines(), generated_tags):
-            f_out.write('{} {}\n'.format(i, j))
+        # new_list = []
+        # to_write = []
+        # for i, j in zip(words[1:-1], generated_tags):
+        #     new_list.append('{} {}'.format(i, j))
+        # for k in new_list:
+        #     if k != ' ':
+        #         to_write.append(k)
+        #
+        # for i in to_write:
+        #     f_out.write('{}\n'.format(i))
+
+        for i, j in zip(words[1:], generated_tags):
+            # f_out.write('{} {}\n'.format(i, j))
+            if i == '\n':
+                f_out.write('{}'.format(i))
+            else:
+                f_out.write('{} {}\n'.format(i, j))
+
+        f_out.close()
+        f_in.close()
 
 
 d = Set()
@@ -185,8 +170,8 @@ d.set_training('./CN/train')
 hmm = HMM(d)
 
 # To train model and save parameters
-hmm.train_trans_params()
-hmm.transi_params.to_pickle("./CN/y_params.pkl")
+# hmm.train_trans_params()
+# hmm.transi_params.to_pickle("./CN/y_params.pkl")
 
 # Load trained parameters
 df_x = pd.read_pickle("./CN/params.pkl")
