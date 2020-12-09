bd648fe3cbdb51030b4b7af25dea2d71e8228903
Doreen <doreen.nps@gmail.com>
2020-12-09 15:56:23 +0800

    Removed old viterbi and useless function

--- part3.py
+++ part3.py
@@ -97,25 +97,6 @@ class HMM:
         self.emission_params = dfx
         self.transistion_params = dfy
 
-    def max_b(self, word):
-        x = word
-
-        if x in self.words:
-            x1 = x
-        else:
-            x1 = '#UNK#'
-
-        row = self.emission_params.loc[self.emission_params['words'] == x1]
-        probs = row['params'].values[0]
-
-        return probs
-
-    def max_a(self, prev_y):
-        row = self.transistion_params.loc[self.transistion_params['tags'] == prev_y]
-        probs = row['y_params'].values[0]
-
-        return probs
-
     def get_sentence(self, in_file):
         f_in = open(in_file, 'r', encoding="utf-8")
         sentence = []
@@ -183,8 +164,6 @@ class HMM:
         tag = self.transistion_params.iloc[pos].tags
         return tag
 
-
-
     def viterbi2(self, in_file, out_file):
         sentences = self.get_sentence(in_file)
         f_out = open(out_file, 'w', encoding="utf-8")
@@ -232,89 +211,14 @@ class HMM:
             f_out.write("\n")
 
 
-
-
-    def viterbi(self, in_file, out_file):
-        f_in = open(in_file, 'r', encoding="utf-8")
-        # f_out = open(out_file, 'w', encoding="utf-8")
-        print('Viterbi running...')
-
-        words = ['\n']
-        lines = f_in.readlines()
-        for i in lines:
-            if i == '\n':
-                words.append(i)
-            else:
-                words.append(i.strip())
-
-        newtags_list = []
-
-        for i in range(len(words)):
-            if words[i] == '\n':
-                scores = []
-                pi_j = 1
-                some_tag = 'S'
-                for j in range(i, len(words) - 1):
-
-                    a_probs = self.max_a(some_tag)
-                    b_probs = self.max_b(words[j+1])
-
-
-                    ab = []
-                    for k, l in zip(a_probs, b_probs):
-                        ab.append(k*l)
-                    pi_j = pi_j * max(ab)
-
-                    pos = np.argmax(ab)
-                    print(words[j + 1], pos)
-                    new_tag = self.tags[pos]
-                    print(words[j + 1], new_tag)
-                    newtags_list.append(new_tag)
-
-                    some_tag = new_tag
-                    scores.append(pi_j)
-
-                    if words[j+2] == '\n':
-                        last_score = max(self.max_a(some_tag))
-                        pi_n1 = pi_j * last_score
-                        scores.append(pi_n1)
-                        pos = np.argmax(self.max_a(some_tag))
-                        newtags_list.append(self.tags[pos])
-                        break
-
-        generated_tags = ['' if k == 'S' else k for k in newtags_list]
-        # new_list = []
-        # to_write = []
-        # for i, j in zip(words[1:-1], generated_tags):
-        #     new_list.append('{} {}'.format(i, j))
-        # for k in new_list:
-        #     if k != ' ':
-        #         to_write.append(k)
-        #
-        # for i in to_write:
-        #     f_out.write('{}\n'.format(i))
-
-
-        print(generated_tags)
-        # for i, j in zip(words[1:], generated_tags):
-        #     # f_out.write('{} {}\n'.format(i, j))
-        #     if i == '\n':
-        #         f_out.write('{}'.format(i))
-        #     else:
-        #         f_out.write('{} {}\n'.format(i, j))
-
-        # f_out.close()
-        f_in.close()
-
-
 d = Set()
 d.set_training('./SG/train')
 
 hmm = HMM(d)
 
 # To train model and save parameters
-hmm.train_trans_params()
-hmm.transi_params.to_pickle("./SG/y_params.pkl")
+# hmm.train_trans_params()
+# hmm.transi_params.to_pickle("./SG/y_params.pkl")
 
 # Load trained parameters
 df_x = pd.read_pickle("./SG/params.pkl")
